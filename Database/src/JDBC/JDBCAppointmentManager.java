ca2455f9ce269ef39f20914ad9391ad853034f27
Meriiig <maria.gilopalomino@usp.ceu.es>
2026-04-28 20:07:08 +0200

    Update JDBCAppointmentManager.java

--- Database/src/JDBC/JDBCAppointmentManager.java
+++ Database/src/JDBC/JDBCAppointmentManager.java
@@ -1,5 +1,28 @@
 package JDBC;
 
-public class JDBCAppointmentManager {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
 
+import Clases.Doctor;
+import Enums.Turn;
+
+public class JDBCAppointmentManager {
+	
+	 private Connection c;
+
+	    public JDBCAppointmentManager(Connection c) {
+	        this.c = c;
+	    }
+
+	
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 }
+
