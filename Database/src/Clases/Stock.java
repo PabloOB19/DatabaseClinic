ec78d1bae806d2ccf7c2ee5bcb194c3aecc9449c
PabloOB19 <pablo.olmoblasco@usp.ceu.es>
2026-04-28 17:12:54 +0200

    kk cosas

--- Database/src/Clases/Stock.java
+++ Database/src/Clases/Stock.java
@@ -5,6 +5,7 @@ import Enums.Type_of_material;
 
 public class Stock {
 	
+	//kk2
 	private int reference_code;
 	private Type_of_material type;
 	private int amount;
--- Database/src/Enums/Paco.java
+++
@@ -1,6 +0,0 @@
-package Enums;
-
-public enum Paco {
-	PACO,
-	PEREZ
-}
