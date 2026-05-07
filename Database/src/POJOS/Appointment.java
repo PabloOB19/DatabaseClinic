e6d21f8a06405eb06cb06502a4e365313cafdc1e
Meriiig <maria.gilopalomino@usp.ceu.es>
2026-05-07 11:43:40 +0200

    Update Appointment.java

--- Database/src/POJOS/Appointment.java
+++ Database/src/POJOS/Appointment.java
@@ -14,19 +14,21 @@ public class Appointment {
     private LocalDate date;
     private Turn turn;
     private double price;
+    private Doctor doctor;
     
     public Appointment () {
 
     }
     
     public Appointment(int id, Type_of_appointment type, LocalDate date,
-            Turn turn, double price) 
+            Turn turn, double price, Doctor doctor) 
 {
     	this.id = id;
         this.type = type;
         this.date = date;
         this.turn = turn;
         this.price = price;
+        this.doctor=doctor;
         
         
 }
@@ -69,6 +71,14 @@ public class Appointment {
     public void setPrice(double price) {
         this.price = price;
     }
+    
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 
 
     @Override
@@ -76,13 +86,15 @@ public class Appointment {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
         Appointment that = (Appointment) object;
-        return id == that.id && Double.compare(price, that.price) == 0 && type == that.type && Objects.equals(date, that.date) && turn == that.turn;
+        return id == that.id && Double.compare(price, that.price) == 0 && type == that.type 
+        	&& Objects.equals(date, that.date) && turn == that.turn 
+        	&& Objects.equals(doctor, that.doctor) ;
     }
 
     
     @Override
     public int hashCode() {
-        return Objects.hash(id, type, date, turn, price);
+        return Objects.hash(id, type, date, turn, price, doctor);
     }
     
     @Override
@@ -93,6 +105,7 @@ public class Appointment {
                 ", date=" + date +
                 ", turn=" + turn +
                 ", price=" + price +
+                 ", doctor=" + doctor +
                 '}';
     }
 
